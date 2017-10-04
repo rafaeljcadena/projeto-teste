9cd94b3eb13f3ea4cca161949b92b297ba35b496
rafaeljcadena <rafaeljcadena@gmail.com>
2017-10-04 01:20:49 -0300

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  title = 'Gerenciador de Tarefas';
+  user = 'Rafael';
+
+  task: Task = new Task(20, 'Enviar orçamento!');
 }
+
+export class Task{
+	public id: number;
+	public title: string;
+
+	constructor(id: number, title: string){
+		this.id = id;
+		this.title = title;
+	}
+}
+
